cb4c7c8965fa5e885fa628b26efa36bd51dfa027
xuyan <2133995120@qq.com>
2025-12-05 14:09:54 +0800

    feat: icmp protocol

--- src/icmp.c
+++ src/icmp.c
@@ -10,7 +10,20 @@
  * @param src_ip 源ip地址
  */
 static void icmp_resp(buf_t *req_buf, uint8_t *src_ip) {
-    // TO-DO
+    buf_init(&txbuf, req_buf->len);
+    icmp_hdr_t *req_icmp_hdr = (icmp_hdr_t *)req_buf->data;
+    icmp_hdr_t *icmp_hdr = (icmp_hdr_t *)txbuf.data;
+    memcpy(txbuf.data, req_buf->data, req_buf->len);
+
+    // buf_remove_header(req_buf, sizeof(icmp_hdr_t));
+    // buf_add_header(&txbuf, sizeof(icmp_hdr_t));
+    memset(icmp_hdr, 0, sizeof(icmp_hdr_t));
+    icmp_hdr->type = ICMP_TYPE_ECHO_REPLY;
+    icmp_hdr->id16 = req_icmp_hdr->id16;
+    icmp_hdr->seq16 = req_icmp_hdr->seq16;
+    icmp_hdr->checksum16 = swap16(checksum16((uint16_t *)txbuf.data, txbuf.len));
+
+    ip_out(&txbuf, src_ip, NET_PROTOCOL_ICMP);
 }
 
 /**
@@ -20,7 +33,13 @@ static void icmp_resp(buf_t *req_buf, uint8_t *src_ip) {
  * @param src_ip 源ip地址
  */
 void icmp_in(buf_t *buf, uint8_t *src_ip) {
-    // TO-DO
+    if (buf->len < sizeof(icmp_hdr_t)) {
+        return;
+    }
+    icmp_hdr_t *icmp_hdr = (icmp_hdr_t *)buf->data;
+    if (icmp_hdr->type == ICMP_TYPE_ECHO_REQUEST) {
+        icmp_resp(buf, src_ip);
+    }
 }
 
 /**
@@ -31,7 +50,20 @@ void icmp_in(buf_t *buf, uint8_t *src_ip) {
  * @param code icmp code，协议不可达或端口不可达
  */
 void icmp_unreachable(buf_t *recv_buf, uint8_t *src_ip, icmp_code_t code) {
-    // TO-DO
+    ip_hdr_t *recv_ip_hdr = (ip_hdr_t *)recv_buf->data;
+    size_t icmp_data_len = recv_ip_hdr->hdr_len * IP_HDR_LEN_PER_BYTE + 8;
+
+    buf_init(&txbuf, sizeof(icmp_hdr_t) + icmp_data_len);
+    memcpy(txbuf.data + sizeof(icmp_hdr_t), recv_buf->data, icmp_data_len);
+
+    icmp_hdr_t *icmp_hdr = (icmp_hdr_t *)txbuf.data;
+    memset(icmp_hdr, 0, sizeof(icmp_hdr_t));
+
+    icmp_hdr->type = ICMP_TYPE_UNREACH;
+    icmp_hdr->code = code;
+    icmp_hdr->checksum16 = swap16(checksum16((uint16_t *)txbuf.data, txbuf.len));
+
+    ip_out(&txbuf, src_ip, NET_PROTOCOL_ICMP);
 }
 
 /**
